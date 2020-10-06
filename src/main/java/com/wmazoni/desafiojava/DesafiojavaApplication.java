b819b68f706561d79cb6ddf2279833292c10af1f
Wellington Andrade <wmazoni@protonmail.com>
2020-10-06 18:11:28 -0300

    Project created

---
+++ src/main/java/com/wmazoni/desafiojava/DesafiojavaApplication.java
@@ -0,0 +1,13 @@
+package com.wmazoni.desafiojava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafiojavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafiojavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wmazoni/desafiojava/DesafiojavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wmazoni.desafiojava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafiojavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
